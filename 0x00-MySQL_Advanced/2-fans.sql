efaac3ca8e35106befef120d6c68c5d2bfec2450
root <root@Reem.localdomain>
2024-10-17 11:29:04 +0300

    fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,1 +1,10 @@
 -- This script ranks country origins of bands, ordered by the number of non-unique fansSELECT     origin,     COUNT(*) AS nb_fansFROM     bands  -- Replace 'bands' with the actual table name if differentGROUP BY     originORDER BY     nb_fans DESC;
+SELECT 
+    origin, 
+    SUM(nb_fans) AS nb_fans
+FROM 
+    metal_bands
+GROUP BY 
+    origin
+ORDER BY 
+    nb_fans DESC;
