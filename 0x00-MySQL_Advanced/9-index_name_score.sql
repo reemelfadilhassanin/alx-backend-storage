d666123920e261a94887e8b3d56ff4c8db907555
root <root@Reem.localdomain>
2024-10-17 14:04:36 +0300

    avarge

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,5 +1,4 @@
 -- 9-index_name_score.sql
 
-DROP INDEX IF EXISTS idx_name_first_score ON names;
-
-CREATE INDEX idx_name_first_score ON names (name(1), score);
+CREATE INDEX idx_name_first_score
+ON names(name(1), score);
