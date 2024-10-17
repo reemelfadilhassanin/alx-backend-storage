06058bd176fe7b7150c09daee7b0a1302a342bf9
root <root@Reem.localdomain>
2024-10-17 11:33:40 +0300

    buy buy

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,16 @@
+-- 4-store.sql
+
+DELIMITER //
+
+CREATE TRIGGER after_order_insert
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+
+//
+
+DELIMITER ;
