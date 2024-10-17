f06d20da278b3043ce43922ef3f6fdf7e1d1672e
root <root@Reem.localdomain>
2024-10-17 14:07:58 +0300

    need meeting

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,9 @@
+-- 11-need_meeting.sql
+
+DROP VIEW IF EXISTS need_meeting;
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+  AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
