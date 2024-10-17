b5816e6714b1054a991ee3d3936ab5b3d97b03e3
root <root@Reem.localdomain>
2024-10-17 14:00:53 +0300

    avarge

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,24 +1,15 @@
 -- 7-average_score.sql
 
-DELIMITER //
+DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser (
-    IN user_id INT
-)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
 
-    -- Calculate the average score for the specified user
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Update the user's average score in the users table
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
-END;
-
-//
+END $$
 
 DELIMITER ;
